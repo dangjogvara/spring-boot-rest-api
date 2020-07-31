9e5555e948869678cdff27aee45c7b81cff395e6
Dan Poulsen <dangjogvara@hotmail.com>
2020-07-31 18:01:21 +0100

    deletePerson and updatePerson resources

--- demo-rest-api/src/main/java/com/dangjogvara/demo/api/PersonController.java
+++ demo-rest-api/src/main/java/com/dangjogvara/demo/api/PersonController.java
@@ -5,9 +5,11 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +43,16 @@ public class PersonController {
 	public Person getPersonByid(@PathVariable("id") UUID id) {
 		return personService.getPersonById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found"));
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public void deletePerson(@PathVariable("id") UUID id) {
+		personService.deletePerson(id);
+	}
+
+	@PutMapping("/{id}")
+	public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person newPerson) {
+		personService.updatePerson(id, newPerson);
 	}
 
 }
