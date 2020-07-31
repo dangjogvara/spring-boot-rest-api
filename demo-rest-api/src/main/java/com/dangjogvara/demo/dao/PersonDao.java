7a5260115b220bb935cd075bf058f1704177ab05
Dan Poulsen <dangjogvara@hotmail.com>
2020-07-31 16:23:38 +0100

    interface getAllPeople

--- demo-rest-api/src/main/java/com/dangjogvara/demo/dao/PersonDao.java
+++ demo-rest-api/src/main/java/com/dangjogvara/demo/dao/PersonDao.java
@@ -1,17 +1,19 @@
 package com.dangjogvara.demo.dao;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.dangjogvara.demo.model.Person;
 
 public interface PersonDao {
 
-	// Insert person object
 	int insertPerson(UUID id, Person person);
 
 	default int insertPerson(Person person) {
 		UUID id = UUID.randomUUID();
 		return insertPerson(id, person);
 	}
+
+	List<Person> selectAllPeople();
 
 }
