8ca3085cb76f6f0554bac891c9bc0107fc8e0338
Dan Poulsen <dangjogvara@hotmail.com>
2020-07-31 16:03:23 +0100

    implement spring features

--- demo-rest-api/src/main/java/com/dangjogvara/demo/service/PersonService.java
+++ demo-rest-api/src/main/java/com/dangjogvara/demo/service/PersonService.java
@@ -1,13 +1,19 @@
 package com.dangjogvara.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
 import com.dangjogvara.demo.dao.PersonDao;
 import com.dangjogvara.demo.model.Person;
 
+@Service
 public class PersonService {
 
 	public PersonDao personDao;
 
-	public PersonService(PersonDao personDao) {
+	@Autowired
+	public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
 		this.personDao = personDao;
 	}
 
